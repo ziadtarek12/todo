5404e9a4fa048a8b09fecf810c7478cd944975fb
ziadtarek12 <zi3dtarek@gmail.com>
2023-04-25 10:14:48 +0200

    Priority sorting, Add css with bootstrap

--- todo/app1/urls.py
+++ todo/app1/urls.py
@@ -2,7 +2,7 @@
 from django.urls import path
 from . import views
 
-    
+app_name = "todo"    
 urlpatterns = [
     path("", views.index, name="index"),
     path("add", views.add, name="add")
--- todo/app1/views.py
+++ todo/app1/views.py
@@ -1,9 +1,40 @@
 from django.shortcuts import render
+from django import forms
+from django.urls import reverse
+from django.http import HttpResponseRedirect
 
 # Create your views here.
+class NewTaskForm(forms.Form):
+    task = forms.CharField(label="New Task", widget=forms.TextInput(attrs={"class" : "form-control"}))
+    priority = forms.IntegerField(min_value = 1, max_value = 10, label="Priority", widget=forms.TextInput(attrs={'class': 'form-control'}))
+
 
 def index(request):
-    return render(request, "app1/index.html")
+    
+    if "tasks" not in request.session:
+        request.session["tasks"] = []
+
+    request.session["tasks"] = sorted(request.session["tasks"], key= lambda x: x["Priority"], reverse=True)
+    return render(request, "app1/index.html", context={
+        "tasks" : request.session["tasks"]
+    })
 
 def add(request):
-    return render(request, "app1/add.html")
+    if request.method == "POST":
+
+        user_form = NewTaskForm(request.POST)
+        if user_form.is_valid():
+            task = user_form.cleaned_data["task"]
+            priority = user_form.cleaned_data["priority"]
+
+            request.session["tasks"] += [{"Task" : task, "Priority" : priority}]
+
+            return HttpResponseRedirect(reverse("todo:index"))
+        else:
+            return render(request, "app1/add.html", context={
+                "form" : user_form
+            })    
+
+    return render(request, "app1/add.html", context={
+        "form" : NewTaskForm()
+    })
